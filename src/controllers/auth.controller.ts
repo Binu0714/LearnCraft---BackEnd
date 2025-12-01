3097186d8f87ceec159ea00586448b81993e0061
Binu0714 <binuj714@gmail.com>
2025-12-01 22:47:01 +0530

    create login function

--- src/controllers/auth.controller.ts
+++ src/controllers/auth.controller.ts
@@ -69,3 +69,62 @@ export const handleRefreshToken = async (req: Request, res: Response) => {
     res.status(500).json({ message: "Invalid or expire token" })
   }
 }
+
+export const login = async (req: Request, res: Response) => {
+    try{
+        const {email, password} = req.body
+
+        if(!email || !password){
+            return res.status(400).json({message: "All fields are required"})
+        }
+
+        const existingUser = await User.findOne({email})
+        if(!existingUser){
+            return res.status(404).json({message: "User not found"})
+        }
+
+        const valid = await bcrypt.compare(password, existingUser.password)
+        if(!valid){
+            return res.status(401).json({message: "Invalid credentials"})
+        }
+
+        const accessToken = signAccessToken(existingUser)
+        const refreshToken = signRefreshToken(existingUser)
+
+        res.status(200).json({
+            message: "Login successful",
+            data: {
+                email: existingUser.email,
+                accessToken,
+                refreshToken
+            }
+        })
+    } catch (err: any) {
+        res.status(500).json({message: "Internal server error"})
+    }
+}
+
+export const getMyDetails = async (req: AuthRequest, res: Response) => {
+    if (!req.user) {
+        return res.status(401).json({ message: "Unauthorized" })
+    }
+
+    const userId = req.user.sub
+
+    const user = ((await User.findById(userId).select("-password")) as IUser) || null
+
+    if (!user) {
+        return res.status(404).json({ message: "User not found" })
+    }
+
+    const { _id, username, email } = user
+
+    res.status(200).json({
+        message: "User details fetched successfully",
+        data: {
+            id: _id,
+            username,
+            email
+        }
+    })
+}
--- src/routes/auth.routes.ts
+++ src/routes/auth.routes.ts
@@ -1,6 +1,8 @@
 import { Router } from "express"
 import {
+    getMyDetails,
   handleRefreshToken,
+  login,
   register
 } from "../controllers/auth.controller"
 import { authenticate } from "../middleware/auth"
@@ -9,5 +11,10 @@ const router = Router()
 
 router.post("/register", register)
 router.post("/refresh", handleRefreshToken)
+router.post("/login", login)
+
+router.get("/me", authenticate, getMyDetails)
+
+
 
 export default router
